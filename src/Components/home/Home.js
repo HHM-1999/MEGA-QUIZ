7883fd08693fb35e9e1cd8299d94d9461bcf8db7
HHM-1999 <hhm639069@gmail.com>
2022-10-11 02:38:29 +0530

    2nd commit

--- src/Components/home/Home.js
+++ src/Components/home/Home.js
@@ -5,10 +5,14 @@ const Home = () => {
     return (
        
         <div className='container mt-5 home '>
-            <img src='https://img.freepik.com/free-photo/person-holding-light-bulb-with-graduation-cap_23-2148721299.jpg?w=996&t=st=1665432396~exp=1665432996~hmac=67b79e322d848b2eda4c115f5bfe233279c91cc080d955e0b93f21e328f6cdda' alt=''></img>
+            <img  src='https://img.freepik.com/free-photo/person-holding-light-bulb-with-graduation-cap_23-2148721299.jpg?w=996&t=st=1665432396~exp=1665432996~hmac=67b79e322d848b2eda4c115f5bfe233279c91cc080d955e0b93f21e328f6cdda' alt=''></img>
 
-            <p className='mt-4'><span className='text-info fs-4'>“Education is the most powerful weapon which you can use to change the world” – Nelson Mandela Education is the first step for people to gain the knowledge, critical thinking, empowerment and skills they need to make this world a better place.</span>
-            
+            <p className='mt-4 fs-4'><span className='text-info fs-3'>“Education is the most powerful weapon which you can use to change the world” – Nelson Mandela Education is the first step for people to gain the knowledge, critical thinking, empowerment and skills they need to make this world a better place.</span><br></br>
+                As We know that Quizzes encourage pupils' self awareness of progress and self assessment. By taking quizzes, pupils get instant feedback on their responses. And this can help them identify areas they need to develop themselves and highlight progress for them to be proud of. <br></br> So We arrange a Perfect platform for all active Students of different topics quizzes. The Topics are React,Html,Css,Javascript. Students can give quizzes and gaining knowledge with giving several types of MCQ Question in Various Topics . Also they will get Marks immediately which will increasing their Confidence  day by day. <br></br>
+                <br></br>
+
+
+                <span className='mt-5 fs-2 text-primary' >So Guys!!! Checkout all these question and set yourself in topmost level. All the Best...... </span>
             </p>
         </div>
     );
